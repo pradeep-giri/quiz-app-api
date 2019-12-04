f488985a2f877538c34bce9b3f652ec17dec74c9
unknown <pradeep.g@holorobotics.com>
2019-12-04 13:46:04 +0530

    letest final

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/images', express.static(path.join('backend/images')));
+app.use('/images', express.static(path.join('images')));
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
